992fcb8f4b58936e6e092b39799658e04da1a5e4
David Roundy <daveroundy@gmail.com>
2020-08-24 12:45:36 -0700

    working code!!!

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,33 @@ struct Overview {
 impl DisplayAs<HTML> for Overview {}
 
 use warp::Reply;
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+struct Student {
+    name: String,
+    email: String,
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
-struct Submit {
-    csv: String,
+struct Group {
+    name: String,
+    students: Vec<Student>,
 }
+
+#[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
+struct Groups {
+    title: String,
+    min_students: usize,
+    students: Vec<Student>,
+    absent: Vec<Student>,
+    groups: Vec<Group>,
+}
+#[with_template("[%" "%]" "groups.html")]
+impl DisplayAs<HTML> for Groups {}
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
+
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let overview = warp::path!("overview" / String).map(move |name| {
         let o = Overview { course_name: name };
@@ -28,31 +47,115 @@ async fn main() {
 
     use bytes::BufMut;
     use futures::{TryFutureExt, TryStreamExt};
-    let submit = warp::path!("submit").and(warp::multipart::form()).and_then(|form: warp::multipart::FormData| {
-        async {
-            // Collect the fields into (name, value): (String, Vec<u8>)
-            let part: Result<Vec<(String, Vec<u8>)>, warp::Rejection> = form
-                .and_then(|part| {
-                    println!("at the start?");
-                    let name = part.name().to_string();
-                    let value = part.stream().try_fold(Vec::new(), |mut vec, data| {
-                        vec.put(data);
-                        async move { Ok(vec) }
+    let submit = warp::path!("submit")
+        .and(warp::multipart::form())
+        .and_then(|form: warp::multipart::FormData| {
+            async {
+                // Collect the fields into (name, value): (String, Vec<u8>)
+                let part: Result<Vec<(String, Vec<u8>)>, warp::Rejection> = form
+                    .and_then(|part| {
+                        println!("at the start?");
+                        let name = part.name().to_string();
+                        let value = part.stream().try_fold(Vec::new(), |mut vec, data| {
+                            vec.put(data);
+                            async move { Ok(vec) }
+                        });
+                        println!("I am processing in the middle...");
+                        value.map_ok(move |vec| (name, vec))
+                    })
+                    .try_collect()
+                    .await
+                    .map_err(|e| {
+                        panic!("multipart error: {:?}", e);
                     });
-                    println!("I am processing in the middle...");
-                    value.map_ok(move |vec| (name, vec))
-                })
-                .try_collect()
-                .await
-                .map_err(|e| {
-                    panic!("multipart error: {:?}", e);
-                });
-            part
-        }
-    })
-    .map(|x| {
-        format!("Hello world {:?}", x)
-    });
+                part
+            }
+        })
+        .map(|x: Vec<(String, Vec<u8>)>| {
+            for (a, b) in x.iter() {
+                if a == "csv" {
+                    println!("got some good {:?}", String::from_utf8_lossy(b));
+
+                    let mut data = Groups {
+                        title: "".to_string(),
+                        min_students: 3,
+                        students: Vec::new(),
+                        absent: Vec::new(),
+                        groups: Vec::new(),
+                    };
+                    let s = String::from_utf8_lossy(b);
+                    let mut rdr = csv::ReaderBuilder::new()
+                        .has_headers(false)
+                        .from_reader(s.as_bytes());
+                    for r in rdr.records() {
+                        if let Ok(r) = r {
+                            let x: Vec<String> = r.iter().map(|x| x.to_string()).collect();
+                            if x[0] == "title" {
+                                data.title = x[1].to_string();
+                            } else if x[0] == "minimum" {
+                                if let Ok(m) = x[1].parse::<usize>() {
+                                    data.min_students = m;
+                                }
+                            } else if x[1] != "" {
+                                println!("{}  ->  {}", x[0], x[1]);
+                                if x.len() < 3 || x[2] != "absent" {
+                                    data.students.push(Student {
+                                        name: x[0].to_string(),
+                                        email: x[1].to_string(),
+                                    });
+                                } else {
+                                    data.absent.push(Student {
+                                        name: x[0].to_string(),
+                                        email: x[1].to_string(),
+                                    });
+                                }
+                            }
+                        }
+                    }
+                    let mut rng = rand::thread_rng();
+                    let mut students_left = data.students.clone();
+                    use rand::seq::SliceRandom;
+                    students_left.shuffle(&mut rng);
+                    while students_left.len() > 0 {
+                        if students_left.len() <= data.min_students + 1 {
+                            data.groups.push(Group {
+                                name: memorable_wordlist::camel_case(40),
+                                students: students_left.drain(..).collect(),
+                            })
+                        } else if students_left.len() % (data.min_students + 1) == 0 {
+                            data.groups.push(Group {
+                                name: memorable_wordlist::camel_case(40),
+                                students: students_left.drain(0..data.min_students + 1).collect(),
+                            })
+                        } else {
+                            data.groups.push(Group {
+                                name: memorable_wordlist::camel_case(40),
+                                students: students_left.drain(0..data.min_students).collect(),
+                            })
+                        }
+                    }
+                    let mut absent_left = data.absent.clone();
+                    absent_left.shuffle(&mut rng);
+                    for a in absent_left.into_iter() {
+                        if let Some(smallest) = data.groups.iter().map(|g| g.students.len()).min() {
+                            data.groups
+                                .iter_mut()
+                                .filter(|g| g.students.len() == smallest)
+                                .next()
+                                .unwrap()
+                                .students
+                                .push(a);
+                        }
+                    }
+                    for g in data.groups.iter_mut() {
+                        g.students.shuffle(&mut rng);
+                    }
+                    return display(HTML, &data).into_response();
+                }
+            }
+            return display(HTML, &"Error parsing CSV file".to_string()).into_response();
+        })
+        .with(warp::log("foo"));
 
     // let submit = warp::path!("submit")
     //     .and(warp::body::content_length_limit(1024 * 128))
