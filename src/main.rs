95706b7a6f99269f965220e3b93cda94c5aa9b5f
David Roundy <daveroundy@gmail.com>
2020-08-27 17:31:41 -0700

    randomize the order, but not of the groups

--- src/main.rs
+++ src/main.rs
@@ -215,6 +215,10 @@ async fn main() {
                         data.groups.sort_by_cached_key(badness);
                         data.groups[0].students.push(s);
                     }
+                    for g in data.groups.iter_mut() {
+                        g.students.shuffle(&mut rng);
+                    }
+                    data.groups.sort_by_cached_key(|g| g.num);
                     use std::fmt::Write;
                     let mut zoom = String::new();
                     writeln!(&mut zoom, "\n\nPre-assign Room Name,Email Address").ok();
